b4763d1af240e9a1cc7a7bf95d5a04bd67243733
Marcin Gołda <marcon1993@gmail.com>
2016-12-28 23:02:06 +0100

    Gabinety

--- src/main/java/pl/edu/pk/bean/Room.java
+++ src/main/java/pl/edu/pk/bean/Room.java
@@ -1,5 +1,37 @@
 package pl.edu.pk.bean;
 
-public class Room {
+import javax.persistence.Entity;
 
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import pl.edu.pk.bean.enums.VisitType;
+
+@Entity
+@Table(name = "room")
+public class Room {
+	@Id
+	private int roomNumber;
+	
+	@NotNull
+	private VisitType roomType;
+
+	public int getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(int roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public VisitType getRoomType() {
+		return roomType;
+	}
+
+	public void setRoomType(VisitType roomType) {
+		this.roomType = roomType;
+	}
+
+	
 }
